e8c4b5fdc7853df549e58036c74d0ea93409e9d1
Parker Strauss <straussp@gilmour.org>
2019-10-11 10:50:16 -0400

    added servo movement

--- student.py
+++ student.py
@@ -85,6 +85,7 @@ class Piggy(PiggyParent):
         time.sleep(1)
         self.servo(1000) #look left
         self.servo(2000) #look right
+        self.servo(1000) #look left
         time.sleep(1)
         self.right()
         time.sleep(1)
