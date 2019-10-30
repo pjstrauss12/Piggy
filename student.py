3576b0faa4506e7eac0d15b20315f41421920e34
Parker Strauss <straussp@gilmour.org>
2019-10-30 14:41:08 -0400

    made obstacle_count and start nav

--- student.py
+++ student.py
@@ -133,13 +133,32 @@ class Piggy(PiggyParent):
             self.scan_data[angle] = self.read_distance()
 
     def obstacle_count(self):
-        print("I can't count how many obstacles are around me. Please give my programmer a zero.")
+        """Does a 360 scan and returns the number of obstacles it sees"""
+        found_something = False #trigger
+        trigger_distance = 250
+        count = 0
+        starting_position = self.get_heading() #write down starting position
+        self.right(primary=60, counter=-60)
+        while self.get_heading() != starting_position:
+            if self.read_distance < trigger_distance and not found_something:
+                found_something = True
+                count += 1
+                print("\n FOUND SOMETHING \n")
+            elif self.read_distance() > trigger_distance and found_something:
+                found_something = False
+                print("I have a clear view.  Resetting my counter")
+        self.stop()
+        print("I have found this many things: %d" % count)
+        return count
 
     def nav(self):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
-        print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
+        while self.read_distance() > 250:
+            self.fwd()
+            time.sleep(.01)
+        self.stop()
 
 
 
