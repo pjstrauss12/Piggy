24f60027dffbab86fd52f51837e53b7ae25e6fed
Parker Strauss <straussp@gilmour.org>
2019-11-15 13:17:46 -0500

    bug fix: midpoint

--- student.py
+++ student.py
@@ -21,7 +21,7 @@ class Piggy(PiggyParent):
         self.RIGHT_DEFAULT = 80
         self.SAFE_DIST = 350
         self.corner_count = 0
-        self.MIDPOINT = 1200  # what servo command (1000-2000) is straight forward for your bot?
+        self.MIDPOINT = 1600  # what servo command (1000-2000) is straight forward for your bot?
         self.load_defaults()
         
 
