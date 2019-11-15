1c08d44f806cec96c1b6b3d3692c74fe363676b4
Parker Strauss <straussp@gilmour.org>
2019-11-15 13:51:01 -0500

    scan method fix

--- student.py
+++ student.py
@@ -130,7 +130,7 @@ class Piggy(PiggyParent):
 
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 50):
+        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 70):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
 
