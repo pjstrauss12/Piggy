a0f579a36c3781f5a61cae99d1f46e97295fb5a5
Parker Strauss <straussp@gilmour.org>
2019-11-18 11:19:34 -0500

    adjusted scan servo movement

--- student.py
+++ student.py
@@ -130,7 +130,7 @@ class Piggy(PiggyParent):
 
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 300):
+        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 200):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
 
