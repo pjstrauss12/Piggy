36fdd71f5c03276385bd6bec27f86166dac69829
Parker Strauss <straussp@gilmour.org>
2019-11-18 11:06:51 -0500

    changed range of scan

--- student.py
+++ student.py
@@ -130,7 +130,7 @@ class Piggy(PiggyParent):
 
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 70):
+        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 300):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
 
