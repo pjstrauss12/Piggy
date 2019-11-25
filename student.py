976284d4e2929597b033adf982828a8caeb3920b
Parker Strauss <straussp@gilmour.org>
2019-11-25 13:58:57 -0500

    adjested all comments

--- student.py
+++ student.py
@@ -17,10 +17,10 @@ class Piggy(PiggyParent):
         ''' 
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
-        self.LEFT_DEFAULT = 100
-        self.RIGHT_DEFAULT = 100
-        self.SAFE_DIST = 350
-        self.corner_count = 0
+        self.LEFT_DEFAULT = 100 # motor speed
+        self.RIGHT_DEFAULT = 100 # motor speed
+        self.SAFE_DIST = 350 # how far I have to be from an object to drive
+        self.corner_count = 0 # sets how many times the robot turns before get_me_out is activated
         self.MIDPOINT = 1600  # what servo command (1000-2000) is straight forward for your bot?
         self.load_defaults()
         
@@ -59,10 +59,10 @@ class Piggy(PiggyParent):
 
     def dance(self):
         """full dance"""
-        #check to see it's safe
+        # check to see it's safe
         if not self.safe_to_dance():
             print("Not enough space to dance!")
-            return #return closes down method
+            return # return closes down method
         else:
             print("It's safe to dance!")
         for x in range(3):
@@ -101,9 +101,9 @@ class Piggy(PiggyParent):
     def head_turn(self):
         """turns servo"""
         for x in range(4):
-            self.servo(2000) #look right
+            self.servo(2000) # look right
             time.sleep(1)
-            self.servo(1000) #look left
+            self.servo(1000) # look left
             time.sleep(1)
 
     def rotation(self):
@@ -136,10 +136,10 @@ class Piggy(PiggyParent):
 
     def obstacle_count(self):
         """Does a 360 scan and returns the number of obstacles it sees"""
-        found_something = False #trigger
+        found_something = False # trigger
         trigger_distance = 250
         count = 0
-        starting_position = self.get_heading() #write down starting position
+        starting_position = self.get_heading() # write down starting position
         self.right(primary=60, counter=-60)
         while self.get_heading() != starting_position:
             if self.read_distance < trigger_distance and not found_something:
@@ -159,7 +159,7 @@ class Piggy(PiggyParent):
             self.servo(ang)
             if self.read_distance() < self.SAFE_DIST:
                 return False
-        #if I get to the end, this means I didn't find anything dangerous
+        # if I get to the end, this means I didn't find anything dangerous
         return True        
 
     def nav(self):
@@ -172,22 +172,22 @@ class Piggy(PiggyParent):
         started_at = self.get_heading()
         while True:    
             while self.quick_check():
-                self.corner_count #pulls up current corner_count
-                self.fwd() #goes forward and costantly checks for objects
+                self.corner_count # pulls up current corner_count
+                self.fwd() # goes forward and costantly checks for objects
                 time.sleep(.01)
             self.stop()
             self.scan()
-            self.corner_count += 1 #if there is an object, one will be added to corner_count, see get_me_out method
+            self.corner_count += 1 # if there is an object, one will be added to corner_count, see get_me_out method
             self.look_for_stuff()
             self.get_me_out ()
-            self.corner_count #resets corner_count to 0 so it does not repeatedly go in circles when it wants to turn
+            self.corner_count # resets corner_count to 0 so it does not repeatedly go in circles when it wants to turn
             
     def get_me_out(self):
         """If stuck, turns robot around to get it out of a trap"""
         self.corner_count
-        if self.corner_count > 5: #if robot turns more than 5 times, this method activates where it will turn out of it
+        if self.corner_count > 5: # if robot turns more than 5 times, this method activates where it will turn out of it
             self.turn_by_deg(135)
-            self.corner_count = 0 #resets corner_count so robot will not continuously spin 135 degrees
+            self.corner_count = 0 # resets corner_count so robot will not continuously spin 135 degrees
         
     
     def look_for_stuff(self):
@@ -206,12 +206,12 @@ class Piggy(PiggyParent):
                     left_count += 1
         left_avg = left_total / left_count
         right_avg = right_total / right_count
-        if left_avg > right_avg: #if the left has a longer distance, it will turn left
+        if left_avg > right_avg: # if the left has a longer distance, it will turn left
             self.turn_by_deg(-45) 
         else:
-            self.turn_by_deg(45) #if right distance is greater, it will turn right
+            self.turn_by_deg(45) # if right distance is greater, it will turn right
         self.servo(self.MIDPOINT)
-        self.corner_count #resets corner_count to 0 so robot does not spin too much when unnecessary
+        self.corner_count # resets corner_count to 0 so robot does not spin too much when unnecessary
 
 
 
